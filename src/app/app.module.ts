9f775d9d01dc309cb7e6f264f56ec359ee5c1eac
Alejandro <alex>
2019-01-29 12:53:42 +0100

    Creado componenente user view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserViewComponent } from './user-view/user-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent
   ],
   imports: [
     BrowserModule,
