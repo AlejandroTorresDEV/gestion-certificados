92f45d44e9bd18ea535830e609f990852c735573
Alejandro <alex>
2019-01-31 09:40:56 +0100

    Creado metodo saveUserJira

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  email: string;
+  password: string;
+  url: string;
+  proyecto: string;
+  componente: string;
+
+  successLogin :boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveUserJira(){
+    console.log(this.email,this.password,this.url,this.proyecto,this.componente);
   }
 
 }
