a8b552d9ba6961dcb7c5b0b47711c883bccd6d6c
Alejandro <alex>
2019-01-31 13:09:25 +0100

    Creada tabla y botones de prueba

--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserViewComponent implements OnInit {
 
-  data: {
-    nombre  :"alex"
-  }
+  data : [
+    {
+    nombreCliente : "Ayuntamiento"
+    }
+  ]
 
   constructor() { }
 
