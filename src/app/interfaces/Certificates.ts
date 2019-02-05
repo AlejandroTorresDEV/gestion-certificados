4ccd85d4b17b4a4fdca6da1e4df6a31f57fedf0b
Alejandro <alex>
2019-02-05 14:05:52 +0100

    Creada interfaz de certificados

---
+++ src/app/interfaces/Certificates.ts
@@ -0,0 +1,16 @@
+export interface Certificates {
+    id: string
+    alias: string
+    entidad_emisiora: string
+    numero_de_serie: string
+    subject: string
+    caducidad: string
+    password: string
+    id_orga: string
+    nombre_cliente: string
+    contacto_renovacion: string
+    repositorio: string
+    observaciones: string
+    integration_list: string
+    user_id: string
+}
