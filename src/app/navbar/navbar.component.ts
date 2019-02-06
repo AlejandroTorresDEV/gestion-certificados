fb223e97e8105ab21b338e7240d46bcce99a969d
Alejandro <alex>
2019-02-06 11:51:16 +0100

    Ocultadas ruta a los usuarios que no son admin en el navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,12 @@ import { AuthServiceService } from "../services/auth-service.service";
 })
 export class NavbarComponent implements OnInit {
 
+  isAdmin : boolean;
+
   constructor(private authService:AuthServiceService) { }
 
   ngOnInit() {
+    this.isAdmin = this.authService.isAdmin();
   }
 
 
